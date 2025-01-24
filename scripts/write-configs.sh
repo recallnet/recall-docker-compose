c8d2fd8bc6a7d84cca991832d8d631e3fe360c94
Alexei Samokvalov <alexei@sam701.com>
2025-01-24 16:27:31 +0100

    Fix prometheus data file permissions

--- scripts/write-configs.sh
+++ scripts/write-configs.sh
@@ -62,6 +62,8 @@ rm -rf /workdir/prometheus/etc/rules
 mkdir -p $prom_targets_dir
 mkdir -p /workdir/prometheus/etc/alertmanager
 mkdir -p /workdir/prometheus/etc/rules
+mkdir -p /workdir/prometheus/data
+chown nobody /workdir/prometheus/data
 cp /repo/config/services/alerts.yml /workdir/prometheus/etc/rules/
 envsubst < /repo/config/services/prometheus.yml > /workdir/prometheus/etc/config.yml
 [ ! -z $alertmanager_address ] && echo '[{"targets":["'$alertmanager_address'"]}]' > /workdir/prometheus/etc/alertmanager/instance0.json
