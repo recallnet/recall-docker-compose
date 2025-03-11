59047d8f993365fa622dfba7b9fbc212fe82a6be
Alexei Samokvalov <alexei@sam701.com>
2025-03-11 18:28:34 +0100

    Add config validation

--- scripts/init.sh
+++ scripts/init.sh
@@ -10,7 +10,7 @@ source $current_dir/read-config.sh
 mkdir -p $workdir
 workdir=$(cd $workdir; pwd)
 log_file=$workdir/init.log
-utils_image=${project_name}-init-utils
+utils_image=recall-init-utils
 logged_done="true"
 
 function run-docker {
@@ -20,7 +20,7 @@ function run-docker {
     flags="--network $external_default_network"
   fi
   set -u
-  docker run --name recall-init --rm -it $flags $@
+  docker run --name recall-init --rm -it $flags "$@"
 }
 
 function log-job-name {
@@ -33,13 +33,20 @@ function log-done {
   echo "✅"
 }
 
+function validate-config {
+  run-docker $utils_image -c "cast chain-id --rpc-url $parent_endpoint${parent_endpoint_token:+?token=$parent_endpoint_token}" &>> $log_file
+}
+
 rm -f $log_file
 
+log-job-name "Validate config"
+validate-config
+
 log-job-name "Build utils image"
-docker build -t $utils_image -f $current_dir/utils.Dockerfile $current_dir &>> $log_file
+docker build -t $utils_image --build-arg fendermint_image=$fendermint_image -f $current_dir/utils.Dockerfile $current_dir &>> $log_file
 
 log-job-name "Init CometBFT"
-run-docker --user root -v $workdir/cometbft:/cometbft $cometbft_image "init --home /cometbft" &>> $log_file
+run-docker --user root -v $workdir/cometbft:/cometbft --entrypoint bash $cometbft_image -c "cometbft init --home /cometbft" &>> $log_file
 
 log-job-name "Download genesis file"
 if [ "$cometbft_statesync_enable" == "true" ]; then
--- scripts/utils.Dockerfile
+++ scripts/utils.Dockerfile
@@ -1,5 +1,13 @@
-FROM alpine:3.21
+ARG fendermint_image
+FROM $fendermint_image
 
-RUN apk add --no-cache envsubst curl jq bash
+RUN apt update && apt install -y gettext-base curl jq bash
+
+# Install foundry
+RUN set -ex; \
+  arch=$(uname -m | sed -e s/aarch64/arm64/ -e s/x86_64/amd64/); \
+  curl -Lo /tmp/tt.tgz https://github.com/foundry-rs/foundry/releases/download/stable/foundry_stable_linux_${arch}.tar.gz && \
+  tar xvf /tmp/tt.tgz -C /bin && \
+  rm /tmp/tt.tgz
 
 ENTRYPOINT [ "bash" ]
