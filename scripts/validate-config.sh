48c791ab40f1b00feff4bada6e413c3ba34c51a1
Alexei Samokvalov <alexei@sam701.com>
2025-03-12 10:17:13 +0100

    Check funds on subnet

---
+++ scripts/validate-config.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+current_dir=$(dirname $0)
+source $current_dir/read-config.sh
+
+function fail {
+  echo "❌"
+  echo "ERROR: $1"
+  exit 1
+}
+
+rpc_url="$parent_endpoint${parent_endpoint_token:+?token=$parent_endpoint_token}"
+cast chain-id --rpc-url $rpc_url &> /dev/null
+if [ $? != 0 ]; then
+  fail "configured endpoint $parent_endpoint with provided 'parent_endpoint_token' is invalid"
+fi
+
+set -e
+subnet_rpr_url=$(echo $cometbft_rpc_servers | sed -e s/api\./evm./ -e s/':443'//)
+address=$(cast wallet address $node_private_key)
+balance=$(cast balance --rpc-url $subnet_rpr_url $address)
+if [ $balance == 0 ]; then
+  fail "no funds on subnet for the node address $address"
+fi
